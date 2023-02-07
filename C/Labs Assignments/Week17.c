e01fa3ee628a36c1af9ebe19a99868bd839b848e
jaypopat <contact.jaypopat@gmail.com>
2023-02-07 22:32:24 +0000

    better readability

--- C/Labs Assignments/Week17.c
+++ C/Labs Assignments/Week17.c
@@ -8,7 +8,8 @@ void squareIntArray(int* i1, int len);
 void printIntegerArrayBackwards(int* arr, int len);
 void randomIntArray(int* arr, int len, int max);
 
-int main() {
+int main() 
+{
     //Seeding the random number generator with the current time.
     srand(time(NULL));
     int x = 10, y = 20;
@@ -31,15 +32,18 @@ int main() {
  The function takes a pointer to a double and an integer as arguments. It prints the double array
   pointed to by the pointer. dp pointer to the first element of the array and len - length of the array
  */
-void printDoubleArray(double* dp, int len) {
+void printDoubleArray(double* dp, int len) 
+{
     puts("\nPrinting double array");
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len; i++) 
+    {
         printf("%.1lf ", *(dp+i));
     }
 }
 
   //The function takes two integer pointers as arguments and swaps the values of the integers they point
-void swapIntegerValues(int* i1, int* i2){
+void swapIntegerValues(int* i1, int* i2)
+{
     int temp;
     temp = *i1;
     *i1 = *i2;
@@ -47,16 +51,20 @@ void swapIntegerValues(int* i1, int* i2){
 }
 //The function takes a pointer to an array of integers and the length of the array as arguments and prints the square of each element of the array
 
-void squareIntArray(int* i1, int len){
-    for (int i = 0; i < len;i++) {
+void squareIntArray(int* i1, int len)
+{
+    for (int i = 0; i < len;i++) 
+    {
         printf("%d ",*(i1+i)**(i1+i));
     }
 }
 
 //It prints the elements of an array in reverse order
 
-void printIntegerArrayBackwards(int* arr, int len){
-    for (int i = 0; i < len;i++) {
+void printIntegerArrayBackwards(int* arr, int len)
+{
+    for (int i = 0; i < len;i++) 
+    {
         printf("%d ",*(arr + len - 1 - i));
     }
 }
@@ -64,8 +72,10 @@ void printIntegerArrayBackwards(int* arr, int len){
   It takes an array of integers, a length, and a maximum value, and fills the array with random
   integers between 0 and the maximum value
  */
-void randomIntArray(int* arr, int len, int max){
-    for (int i=0;i<len;i++){
+void randomIntArray(int* arr, int len, int max)
+{
+    for (int i=0;i<len;i++)
+    {
         *(arr + i) = rand() % max;
         printf("%d", *(arr + i));
     }
