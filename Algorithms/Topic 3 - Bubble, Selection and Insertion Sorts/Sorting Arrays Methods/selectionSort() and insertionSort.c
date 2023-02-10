40d2de87e1de2b785c4e75ae65da36178fd4ec53
jaypopat <contact.jaypopat@gmail.com>
2023-02-10 23:02:22 +0000

    improved sorting algos by printing outputs of each

--- Algorithms/Topic 3 - Bubble, Selection and Insertion Sorts/Sorting Arrays Methods/selectionSort() and insertionSort.c
+++ Algorithms/Topic 3 - Bubble, Selection and Insertion Sorts/Sorting Arrays Methods/selectionSort() and insertionSort.c
@@ -1,70 +1,66 @@
-// CT102 Algorithms
-// Selection Sort and Insertion Sort functions
-// 31/01/2023
-
 #include <stdio.h>
 #include <stdlib.h>
-
 
 void insertionSort(int[], int);
 void selectionSort(int[], int);
 
-
 // testing functions in main() with small sample data
-void main() {
+int main() {
+    int n = 10;
+    int A[] = {2, 4, 6, 5, 10, 1, 3, 9, 7, 8};
+    int b[] = {2, 4, 6, 6, 10, 1, 3, 9, 8, 8};
 
-	int n = 10;
-	int A[] = {2, 4, 6, 5, 10, 1, 3, 9, 7, 8};
+    selectionSort(A, n);
+    printf("\n Printing out Sorted array (selection sort):\t");
+    for(int i = 0; i < n; i++) {
+        printf("%d\t", A[i]);
+    }
 
-	selectionSort(A, n);
-	insertionSort(A, n);
+    insertionSort(b, n);
+    printf("\n Printing out Sorted array (insertion sort):\t");
+    for(int i = 0; i < n; i++){
+        printf("%d\t", b[i]);
+    }
 
-	printf("\n Printing out Sorted array:: ");	
-	for(int i = 0; i < n; i++)
-
-		printf("\n %d", A[i]);
+    return 0;
 }
 
 // Selection Sort: integer array arrA [] of size
-void selectionSort(int arrA[], int size) {    
+void selectionSort(int arrA[], int size) {
+    int i, j, min, temp;
 
-	int i, j, min, temp;
+    for (i = 0; i < size - 1; i++) {
+        min = i;
+        //find next smallest
+        for (j = min + 1; j < size; j++) {
+            if (arrA[min] > arrA[j]) {
+                min = j;
+            }
+        } // end j for loop
 
-	for (i = 0; i < size - 1; i++) {
-		min = i;
-		//find next smallest
-		for (j = min + 1; j < size; j++) {
-			if (arrA[min] > arrA[j]) {
-				min = j;
-			}
-		} // end j for loop
-
-		//swap values at locations i and min, if i != min
-		if (i != min) {			
-			temp = arrA[i];
-			arrA[i] = arrA[min];
-			arrA[min] = temp;
-		}
-	} //end outer i for loop
+        //swap values at locations i and min, if i != min
+        if (i != min) {
+            temp = arrA[i];
+            arrA[i] = arrA[min];
+            arrA[min] = temp;
+        }
+    } //end outer i for loop
 }
 
-
 // Insertion Sort: integer array arrA [] of size
-void insertionSort(int arrA[], int size)
-{
-	int i, j, curr;
-	
-	for(i = 1; i < size; i++) {
-		curr  = arrA[i];
-	
-		for(j = i - 1; j >= 0 && curr < arrA[j]; j--) {   //compare
-			//make room ...
-			arrA[j + 1] = arrA[j];
-		}  
-	
-		if (i != j + 1) // if not at the correct position already
-			arrA[j + 1] = curr;
+void insertionSort(int arrA[], int size) {
+    int i, j, curr;
 
-	} // end outer i for
+    for(i = 1; i < size; i++) {
+        curr  = arrA[i];
 
+        for(j = i - 1; j >= 0 && curr < arrA[j]; j--) {   //compare
+            //make room ...
+            arrA[j + 1] = arrA[j];
+        }
+
+        if (i != j + 1) // if not at the correct position already
+            arrA[j + 1] = curr;
+
+    } // end outer i for
 } //return
