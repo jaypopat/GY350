be94c9fdc4a3cc54e045822341c29654854d0708
jaypopat <contact.jaypopat@gmail.com>
2023-02-12 23:27:51 +0000

    updated wrong code week4labs

--- Algorithms/Topic 3 - Bubble, Selection and Insertion Sorts/Sorting Arrays Methods/selectionSort() and insertionSort.c
+++ Algorithms/Topic 3 - Bubble, Selection and Insertion Sorts/Sorting Arrays Methods/selectionSort() and insertionSort.c
@@ -1,38 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int comparisons = 0;
+int swaps = 0;
+
 void insertionSort(int[], int);
 void selectionSort(int[], int);
 
-// testing functions in main() with small sample data
+
 int main() {
+
     int n = 10;
     int A[] = {2, 4, 6, 5, 10, 1, 3, 9, 7, 8};
     int b[] = {2, 4, 6, 6, 10, 1, 3, 9, 8, 8};
 
     selectionSort(A, n);
-    printf("\n Printing out Sorted array (selection sort):\t");
-    for(int i = 0; i < n; i++) {
-        printf("%d\t", A[i]);
-    }
+    printf("\nPrinting out Sorted array (Selection Sort):\t");
+    for(int i = 0; i < n; i++)
+        printf("%d  ", A[i]);
+    printf("\nNumber of comparisons: %d\n", comparisons);
+    printf("Number of swaps: %d\n", swaps);
+
+    comparisons = 0;
+    swaps = 0;
 
     insertionSort(b, n);
-    printf("\n Printing out Sorted array (insertion sort):\t");
+    printf("\nPrinting out Sorted array (Insertion Sort):\t");
     for(int i = 0; i < n; i++){
-        printf("%d\t", b[i]);
+        printf("%d  ", b[i]);
     }
+    printf("\nNumber of comparisons: %d\n", comparisons);
+    printf("Number of swaps: %d\n", swaps);
 
     return 0;
 }
 
+
 // Selection Sort: integer array arrA [] of size
 void selectionSort(int arrA[], int size) {
+
     int i, j, min, temp;
 
     for (i = 0; i < size - 1; i++) {
         min = i;
         //find next smallest
         for (j = min + 1; j < size; j++) {
+            comparisons++;
             if (arrA[min] > arrA[j]) {
                 min = j;
             }
@@ -40,6 +53,7 @@ void selectionSort(int arrA[], int size) {
 
         //swap values at locations i and min, if i != min
         if (i != min) {
+            swaps++;
             temp = arrA[i];
             arrA[i] = arrA[min];
             arrA[min] = temp;
@@ -47,20 +61,29 @@ void selectionSort(int arrA[], int size) {
     } //end outer i for loop
 }
 
+
 // Insertion Sort: integer array arrA [] of size
-void insertionSort(int arrA[], int size) {
+void insertionSort(int arrA[], int size)
+{
     int i, j, curr;
 
     for(i = 1; i < size; i++) {
         curr  = arrA[i];
 
-        for(j = i - 1; j >= 0 && curr < arrA[j]; j--) {   //compare
-            //make room ...
-            arrA[j + 1] = arrA[j];
+        for(j = i - 1; j >= 0; j--) {
+            comparisons++;
+            if (curr < arrA[j]) {   //compare
+                //make room ...
+                swaps++;
+                arrA[j + 1] = arrA[j];
+            }
+            else
+                break;
         }
 
         if (i != j + 1) // if not at the correct position already
             arrA[j + 1] = curr;
 
     } // end outer i for
+
 } //return
--- C/Labs Assignments/Week04.c
+++ C/Labs Assignments/Week04.c
@@ -9,55 +9,47 @@
 4. Keep track of the oldest and youngest child. Print the ages of each to the screen at the end. (20 marks)
 
 5. If an age entered is 18 or greater. Print a message that the age entered is not for a child. Do not count this age when determining the average age, the maximum age or the number of children in the family. */
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #include <stdio.h>
-void main() {
 
+int main(void) {
+  int age = 0;
+  int count = 0;
+  float sum_age = 0;
+  float average_age = 0;
+  int maximum_age = 0;
+  int minimum_age = 1000000;
 
-	int num = 100000;
-	int i = 0;
-	int age = 0;
-	int oldest = 0;
-	int youngest = 1000;
-	float sum_age = 0;
-	float average_age = 0;
-	int maximum_age = 0;
-	int minimum_age = 1000;
+  while (1) {
+    printf("Enter the age of the child %d: ", count + 1);
+    scanf("%d", &age);
 
+    if (age < 0) {
+      printf("While loop ending\n");
+      break;
+    }
 
-	while (1) {
-		printf("Enter the age of the child %d : ", i + 1); //inputs the age of the child till user inputs a negative integer --(loop)
+    if (age >= 18) {
+      printf("Not a child\n");
+    } else {
+      count++;
+      sum_age += age;
 
-		scanf_s("%d", &age);//scans the age and stores it in the variable age
+      if (age > maximum_age) {
+        maximum_age = age;
+      }
+      if (age < minimum_age) {
+        minimum_age = age;
+      }
+    }
+  }
 
+  average_age = sum_age / count;
 
-		if (age < 0) {
-			printf("While loop ending\n");
-			break; // if value entered is a negative integer, the loop ends
-		}
+  printf("There are %d children\n", count);
+  printf("The average age is %.2f\n", average_age);
+  printf("The maximum age is %d\n", maximum_age);
+  printf("The minimum age is %d\n", minimum_age);
 
-		if (age > 17)
-			printf("Not a child \n"); //if value entered is over 18, it is excluded and not used in further calculations
-
-
-		else
-		{
-			i++;
-			sum_age = sum_age + age; //algorithm to find the sum of the ages(used to find average)
-
-			if (age > maximum_age) {
-				maximum_age = age; // this finds the maximum age in the inputs entered
-			}
-			if (age < minimum_age) {
-				minimum_age = age; //// this finds the minimum age in the inputs entered
-			}
-		}
-
-	}
-	average_age = sum_age / (i); //algorithm to calculate the average - sum of the numbers divided by the number of ages 
-
-	printf("There are %d children\n", i );
-	printf("The average age is %.2f \n", average_age);
-	printf("The maximum age is %d \n", maximum_age);
-	printf("The minimum age is %d \n", minimum_age);
+  return 0;
 }
