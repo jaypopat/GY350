d4d8218cf6df196123adeed4dfe725a863bb3e13
jaypopat <contact.jaypopat@gmail.com>
2023-02-19 15:49:49 +0000

    added null characters to end of string manually to use strlen

--- C/Labs Assignments/Week20.c
+++ C/Labs Assignments/Week20.c
@@ -1,41 +1,59 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
 char dictionary[10000][50];
 char filteredDict[10000][50];
 
-
-
 void main() {
     FILE *fptr;
+    int i =0;
+    int j=0;
     fptr = fopen("C:\\Users\\email\\Desktop\\GY350\\Coding in C\\C-Lion Projects\\Labs Week 18\\dict.txt", "r");
     if (fptr == NULL) {
-        puts("Error Opening File \n Exiting ........");
+        puts("Error Opening File");
         return;
     }
-    for (int i=0;i<10000;i++){
-        for (int j=0;j<20;j++)
-        {
-            fscanf(fptr, "%c", &dictionary[i][j]);
+    while(!feof(fptr)){
+        char c = fgetc(fptr);
+        if (c == '\n'){
+            dictionary[i][j]='\0';
+            i++;
+            j=0;
         }
+        else{
+            dictionary[i][j]= c;
+            j++;
+        }
+
     }
     fclose(fptr);
+    for ( i = 0; i < 10000; i++) {
+        for ( j = 0; j < 50; j++) {
+            if (dictionary[i][j] == '\0') 
+            {
+                break;
+            }
+            printf("%c", dictionary[i][j]);
+        }
+        printf("\n");
+    }
+
+    // after debugging -> found out strlen doesnt work --> strings are not null terminated which is why have to manually add it with code above
+    // but each word is parsed and separated using a newline
 
     int num_filteredWords=0;
-    for (int i = 0; i < 10000; ++i) {
+    for (i = 0; i < 10000; i++) {
         if (strlen(dictionary[i])<4 || strlen(dictionary[i])>7){
-            continue;
+            continue; // skips the strings
         }
         strcpy(filteredDict[num_filteredWords],dictionary[i]);
         num_filteredWords++;
     }
     printf("%d", num_filteredWords);
 
-    for (int i = 0; i <num_filteredWords ; ++i) {
-        for (int j = 0; j <20 ; ++j) {
+    for ( i = 0; i <num_filteredWords ; i++) {
+        for ( j = 0; j <20 ; j++) {
             printf("%c", filteredDict[i][j]);
         }
     }
-
 }
