47477d5c4d84e8117b230463c1941cab3d923b8b
jaypopat <contact.jaypopat@gmail.com>
2023-02-19 14:31:06 +0000

    tried fopen instead of fopen_s

--- C/Labs Assignments/Week20.c
+++ C/Labs Assignments/Week20.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 
 char dictionary[10000][50];
-char adict[10000][50];
+char filteredDict[10000][50];
 
 
 
 void main() {
     FILE *fptr;
-    fopen_s(&fptr, "C:\\Users\\email\\Desktop\\GY350\\Coding in C\\C-Lion Projects\\Labs Week 18\\dict.txt", "r");
+    fptr = fopen("C:\\Users\\email\\Desktop\\GY350\\Coding in C\\C-Lion Projects\\Labs Week 18\\dict.txt", "r");
     if (fptr == NULL) {
         puts("Error Opening File \n Exiting ........");
         return;
@@ -27,14 +27,14 @@ void main() {
         if (strlen(dictionary[i])<4 || strlen(dictionary[i])>7){
             continue;
         }
-        strcpy(adict[num_filteredWords],dictionary[i]);
+        strcpy(filteredDict[num_filteredWords],dictionary[i]);
         num_filteredWords++;
     }
     printf("%d", num_filteredWords);
 
     for (int i = 0; i <num_filteredWords ; ++i) {
         for (int j = 0; j <20 ; ++j) {
-            printf("%c", adict[i][j]);
+            printf("%c", filteredDict[i][j]);
         }
     }
 
