ef1548b49ca61a6373cccf29d60f08cf7b6695bb
jaypopat <contact.jaypopat@gmail.com>
2023-03-02 16:05:04 +0000

    added first step of problem

--- C/Labs Assignments/Week21.c
+++ C/Labs Assignments/Week21.c
@@ -10,3 +10,61 @@ word has at least one syllable, even if the previous rules give a count of 0.
 3. Count all sentences. A sentence is ended by a full stop, colon, semicolon, question mark, or
 exclamation mark.
 4. The index is computed by the following formula:*/
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int indexCalculator();
+
+char alltext[200000];
+char duplicatedText[200000];
+char oneline[1000];
+
+int main() {
+    FILE *file_ptr;
+    fopen_s(&file_ptr, "C:\\Users\\email\\Desktop\\article-irish-times.txt", "r");
+    int word = 0;
+    int sentence = 0;
+
+    if (file_ptr == NULL) {
+        printf("Could not open article\n");
+    } else {
+        alltext[0] = '\0'; // make sure this string is empty
+        while (fgets(oneline, 999, file_ptr) != NULL) {
+            // read the next line and append it (with \n intact)
+            strcat(alltext, oneline);
+        }
+        printf("%s", alltext);
+        fclose(file_ptr);
+    }
+    printf("\n------------------------\n");
+    
+    indexCalculator(); // do this for two text files
+}
+
+int indexCalculator(){
+    int word = 0;
+    int sentence = 0;
+    char *wordToken = strtok(alltext," ");
+    //printf("%s",token);
+    while (wordToken!=NULL){
+        //printf("%s ",token); prints the text again
+        wordToken = strtok(NULL," ");
+        word++;
+    }
+    printf("%d words",word);
+    printf("\n------------------------\n");
+    strcpy(duplicatedText, alltext);
+    char sentenceDelims[] = ".:;?!";
+    char *sentenceToken = strtok(alltext,sentenceDelims);
+    //printf("%s",token);
+    while (sentenceToken!=NULL){
+        //printf("%s ",token); prints the text again
+        sentenceToken = strtok(NULL,sentenceDelims);
+        sentence++;
+    }
+    printf("%d sentences",sentence);
+    return 0;
+}
+
