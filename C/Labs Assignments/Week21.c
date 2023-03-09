06dd4f04225671d2574aaff37f0e1423dc1b47a1
jaypopat <contact.jaypopat@gmail.com>
2023-03-09 21:59:38 +0000

    added correct code for syllable counter

--- C/Labs Assignments/Week21.c
+++ C/Labs Assignments/Week21.c
@@ -109,4 +109,84 @@ void indexCalculator(char articleText[]) {
     /* This is calculating the Flesch Readability Index*/
     float index = 206.835 - 84.6 * (syllableCount / wordCounter) - (1.015 * (wordCounter / sentence));
     printf("%.3f", index);
+} // my attempt -> didnt print out the output
+
+//solution
+#include <stdio.h> 
+#include <string.h>
+#include <ctype.h>
+
+char alltext[200000];
+char oneline[1000];
+
+const char* delimeters = " \n\t,";
+
+int syllablesInWord(char* word);
+
+int main() {
+    FILE *file_ptr;
+    file_ptr = fopen("article-irish-times.txt", "r"); // open for reading
+    //file_ptr = fopen("article-green-eggs-and-ham.txt", "r");
+
+    if (file_ptr == NULL) {
+        printf("Could not open article\n");
+    }
+    else {
+        alltext[0] = '\0'; // make sure this string is empty
+        while (fgets(oneline, 999, file_ptr)!=NULL) {
+            // read the next line and append it (with \n intact)
+            strcat(alltext, oneline);
+        }
+
+        fclose (file_ptr);
+
+        // loop the whole text to count sentences
+        // note that we do this before running strtok since strtok modifies the string itself
+        // (A sentence is ended by a full stop, colon, semicolon, question mark, or exclamation mark)
+        int sentences = 0;
+        int len = strlen(alltext);
+        for (int i=0; i<len; i++) {
+            char c = alltext[i];
+            if (c=='.' || c==':' || c==';' || c=='?' || c=='!')
+                sentences++;
+        }
+
+        // Calculate the Flesch Readability Index
+        char *word = strtok(alltext, delimeters);
+        int syllables = 0;
+        int words = 0;
+        while( word != NULL ) {
+            // loop each word to count them and their syllables
+            words++;
+            syllables += syllablesInWord(word);
+            word = strtok(NULL, delimeters);
+        }
+
+        float flesch = 206.835 - 84.6 * ((float)syllables/words) - 1.015 * ((float)words/sentences);
+
+        printf("Words=%d  Syllables=%d  Sentences=%d\n", words, syllables, sentences);
+        printf("Flesch Readability Index = %f\n", flesch);
+    }
+}
+
+int syllablesInWord(char* word) {
+    // Each group of adjacent vowels (a, e, i, o, u, y) counts as one syllable
+    // However, an "e" at the end of a word doesn't count as a syllable
+    int s = 0;
+    bool inVowelGroup = false;
+    int len = strlen(word);
+    for (int i=0; i<len; i++) {
+        char c = tolower(word[i]);
+        bool isVowel = (c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='y');
+        if (isVowel && !inVowelGroup) {
+            if (i+1<len || c!='e')
+                s++;
+        }
+        inVowelGroup = isVowel;
+    }
+
+    if (s==0) // 0-syllable words not allowed
+        return 1;
+
+    return s;
 }
