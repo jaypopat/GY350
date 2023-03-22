c165df9c0d5fcdc735f7e87b6d873e190be2aee5
jaypopat <contact.jaypopat@gmail.com>
2023-03-22 21:24:26 +0000

    added problem statement for week24 labs

---
+++ C/Labs Assignments/Week24.c
@@ -0,0 +1,24 @@
+//Adventure Game 2 but using dynamic memory allocation
+/*Extend last week’s ”Galway Adventure” assignment to now include objects which you
+can TAKE and DROP.
+• After typing TAKE or DROP, your program should ask
+for the object’s name
+• An object can only be taken if it’s in the current
+location
+• An object can only be dropped if it’s in the player’s
+inventory
+• You can start with your own code or with the
+solution to last week’s work which I provided in
+class
+• Whenever the current location is displayed, a
+list of objects at the location should also be
+displayed.
+• The command INVENTORY should display a list
+of whatever the player is holding
+• The command EXAMINE should display the
+description of the requested object (only if it is
+in the current location or player’s inventory)*/
+
+int main(){
+    
+}
